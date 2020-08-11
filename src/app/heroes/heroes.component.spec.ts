0ca56428d2b550336fa1a45f03ec51267f215b0d
Kha Nguyen <trongkha@yahoo.com>
2020-08-10 21:22:06 -0700

    Isolation tests with dependency

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,36 @@
+import { HeroesComponent } from "./heroes.component";
+import { of } from "rxjs";
+
+describe("HeroesComponnent",()=>{
+    let component: HeroesComponent;
+    let HEROES;
+    let mockHeroService;
+
+    beforeEach(()=>{
+        HEROES= [
+            {id:1,name:'SpiderDude',strength:8},
+            {id:2,name:'Wonderful Woman',strength:24},
+            {id:3,name:'SuperDude',strength:55}
+        ]
+        
+        mockHeroService = jasmine.createSpyObj(['addHero','deleteHero','getHeroes']);
+
+        component = new HeroesComponent(mockHeroService);
+
+    });
+
+    describe('delete', () => {
+        it ('should remove the indicated hero from hero lists', ()=>{
+            //arrange
+            mockHeroService.deleteHero.and.returnValue(of(true));
+            component.heroes = HEROES;
+
+            //act
+            component.delete(HEROES[2]);
+            //assert
+            expect(component.heroes.length).toEqual(2);
+        })
+
+    });
+
+});
