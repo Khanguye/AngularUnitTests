f607c7e390e40269f6ab4ad05bc9c40acce95b34
Kha Nguyen <trongkha@yahoo.com>
2020-08-12 23:30:48 -0700

    testBed using timeout for a long function to finish

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from "@angular/router";
 import { Location } from '@angular/common';
 import { of } from "rxjs";
 import { FormsModule } from "@angular/forms";
+import { timeout } from "rxjs/operators";
 
 describe('HeroDetailComponent',()=>{
     let mockLocation, mockHeroService,mockActivatedRoute;
@@ -31,12 +32,25 @@ describe('HeroDetailComponent',()=>{
         mockHeroService.getHero.and.returnValue(of(
             {id: 3, name: 'SuperDude', strength:100}
         ))
+     
     });
 
     it('Should render hero name in a h2 tag',()=>{
+      
         fixture.detectChanges();
 
         expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SuperDude'.toUpperCase());
     });
 
+    it('should call updateHero when save is called',(done)=>{
+
+        mockHeroService.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.save();
+        setTimeout(()=>{
+             expect(mockHeroService.updateHero).toHaveBeenCalled();
+             done();
+        }, 300);
+    });
 });
